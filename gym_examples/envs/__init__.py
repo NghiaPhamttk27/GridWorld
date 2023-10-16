ddbc1ca44533188e15b7cfa171f93b4f675f91f3
unknown <nghiaphamtrung2709@gmail.com>
2023-10-16 22:40:31 +0700

    first

---
+++ gym_examples/envs/__init__.py
@@ -0,0 +1,9 @@
+from gymnasium.envs.registration import register
+from gym_examples.envs.grid_world import GridWorldEnv
+import gym_examples
+
+register(
+    id="gym_examples/GridWorld-v0",
+    entry_point="gym_examples.envs:GridWorldEnv",
+    max_episode_steps=300,
+)
---
+++ main.py
@@ -0,0 +1,5 @@
+import gymnasium
+import gym_examples
+
+
+env = gymnasium.make('gym_examples/GridWorld-v0')
